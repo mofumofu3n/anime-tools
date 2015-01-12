7e181d3d50675bfe73e831182f9905fc987b24bd
mofumofu3n <mofumofu3n@gmail.com>
2015-01-12 23:38:52 +0900

    feedを読み込む処理を追加

--- tests/AnimeTools/CrawlerTest.php
+++ tests/AnimeTools/CrawlerTest.php
@@ -1,14 +1,33 @@
 <?php
 namespace AnimeTools;
 
+require __DIR__.'/../../config.php';
+
 use AnimeTools\Crawler;
+
+use Parse\ParseClient;
+use Parse\ParseQuery;
+use Parse\ParseException;
 
 class CrawlerTest extends \PHPUnit_Framework_TestCase
 {
 
     public function setup()
     {
-        $this->crawler = new Crawler('test');
+        // Parseの設定読み込み
+        ParseClient::initialize(PARSE_APP_ID, PARSE_REST_KEY, PARSE_MASTER_KEY);
+        
+        // ParseのFeedクラスから取得
+        // TODO たぶんモックにしたほうがいい
+        $query = new ParseQuery("Feed");
+
+        try {
+            $results = $query->find();
+        } catch (ParseException $e) {
+            var_dump($e);
+        }
+
+        $this->crawler = new Crawler('test', $results);
         $this->contents = file_get_contents(__DIR__.'/file/rdf.xml');
     }
 
