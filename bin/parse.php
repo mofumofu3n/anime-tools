55f1ca5faacab841ab03222b479905b63ca927ab
mofumofu3n <mofumofu3n@gmail.com>
2015-01-22 01:29:53 +0900

    設定ファイルのパスを修正

--- bin/parse.php
+++ bin/parse.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../config.php';
+require __DIR__.'/../Config.php';
 
 use AnimeTools\Crawler;
 use AnimeTools\Model\ArticleBuilder;
